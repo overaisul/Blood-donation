d814b099416c13aaa13bc066e8875707a0ca1e9b
overaisul <overaisulislam@gmail.com>
2022-03-14 17:36:50 +0600

    index.php

---
+++ HTML/index.php
@@ -0,0 +1,269 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Donation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/style.css">
+</head>
+<body>  
+
+
+
+    <nav class="navbar">
+        <div class="container">
+                <div class="max">
+                    <a class="nav-link active" aria-current="page" href="#">ROKTO.</a>
+                </div>
+                <ul class="nav justify-content-end">
+                   
+                    <li class="nav-item">
+                      <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="about.php">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="step.php">Steps</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="search.php">Search Donor</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="../admin/admin-log.php">Admin login</a>
+                      </li>
+                  </ul>
+         </div>
+        </nav>
+        <section>
+        <div class="banner-card">
+            <img src="../image/img2.png" alt="">
+            <div class="banner-text">
+                <h3>SMS-based platform </h3>
+                <h4>to connect blood searchers with donors</h4>
+                <p>Rokto is a real-time free platform to help blood searchers connect voluntary  <br>blood donors around Bangladesh.</p>
+                <a href="register.php">
+                <button type="button" class="btn btn-danger btn-sm">Join as a Blood Donor</button></a>
+                <a href="search.php">
+                <button type="button" class="btn btn-light btn-sm">Search Blood Donor</button></a>
+            </div>
+        </div>
+    </section>
+    <div class="container">
+        <div class="what">
+        <div class="what2">
+            <h3>What is Rokto?</h3>
+            <p>Rokto is an automated blood service that connects <br> blood searchers with voluntary donors in a moment through SMS. <br> Rokto is always a free service for all.</p>
+        </div>
+        <div class="how">
+            <h3>Why is Rokto?</h3>
+            <ul>
+                <li>100% automated</li>
+                <li>Always Free</li>
+                <li>24*7 Service</li>
+                <li>Secured</li>
+            </ul>
+        </div>
+        </div>
+        <div class="d-grid gap-1 col-6 mx-auto">
+          <center>
+          <a href="about.php">
+            <button class="btn btn-danger" type="button">Learn More</button>
+          </div></a></center>
+    </div>
+  <div class="container">
+    <div class="msg">
+      <h3>SMS Format</h3>
+      <div class="msg2">
+      <h4>NEED</h4>
+      <p>To contact donor when you need Blood.</p>
+      <p>FORMAT:    need.    group.    district.    unit.    Date</p>
+      <p>EXAMPLE:   need.    O+.       Khulna.      2.       22/02/2022</p>
+    </div>
+  </div>
+  </div>
+  <div class="container">
+    <div class="msg">
+      <h3>We are Network of</h3>
+    </div>
+  <div class="icon">
+    <img src="../image/icon1.png" alt="">
+    <img src="../image/icon2.jpg" alt="">
+    <img src="../image/icon3.png" alt="">
+  </div>
+  <div class="text">
+    <h4>5302 Donor</h4>
+    <h4>64 District</h4>
+    <h4>8 Blood Group</h4>
+  </div>
+</div> 
+<div class="about">
+<div class="container">
+    <h5>About Us</h5>
+    <small>Rokto is an automated blood service that connects blood searchers with voluntary blood donors in a moment through SMS. Rokto is a not-for-profit initiative to aware people of voluntary blood donation in Bangladesh.</small>
+    <div class="d-grid gap-1 col-6 mx-auto">
+      <a href="about.php">
+      <button class="btn btn-danger" type="button">Learn More</button>
+    </div></a>
+  </div>
+</div>
+
+
+
+
+
+<center>
+  <div class="member">
+  <h4>Members</h4></div>
+</center>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="team">
+        <div class="content">
+            <div class="imgbx">
+              <img src="../image/ove.jpg" alt="">
+            </div>
+            <div class="contentbx">
+              <h4>Raisul Islam</h4>
+              <h6>volunteer</h6>
+              <div class="sci">
+                <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png"/></a>
+                  <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v1.png"/></a>
+                  <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/twitter.png"/></a>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="team">
+        <div class="content">
+            <div class="imgbx">
+              <img src="../image/niloy.jpg" alt=""class="niloy">
+            </div>
+            <div class="contentbx">
+              <h4>Samiun Niloy</h4>
+              <h6>volunteer</h6>
+              <div class="sci">
+                <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png"/></a>
+                  <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v1.png"/></a>
+                  <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/twitter.png"/></a>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3"><div class="team">
+      <div class="content">
+          <div class="imgbx">
+            <img src="../image/asif.jpg" alt=""class="asif">
+          </div>
+          <div class="contentbx">
+            <h4>Minhaz Asif</h4>
+            <h6>volunteer</h6>
+            <div class="sci">
+              <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png"/></a>
+                <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v1.png"/></a>
+                <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/twitter.png"/></a>
+            </div>
+          </div>
+      </div>
+    </div></div>
+    <div class="col-md-3"><div class="team">
+      <div class="content">
+          <div class="imgbx">
+            <img src="../image/te3.jpg" alt=""class="rahul">
+          </div>
+          <div class="contentbx">
+            <h4>Rahul Das</h4>
+            <h6>volunteer</h6>
+            <div class="sci">
+              <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png"/></a>
+                <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v1.png"/></a>
+                <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/twitter.png"/></a>
+            </div>
+          </div>
+      </div>
+    </div></div>
+  </div>
+</div>
+</div>
+<br>
+<br>
+
+
+
+
+
+
+
+<div class="contact">
+  <div class="con">
+    <h4>Contact Us</h4>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <ul>
+          <li>
+            <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png"/></a>
+            <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v1.png"/></a>
+            <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/twitter.png"/></a>
+            </li>
+        </ul>
+        <div class="footer2">
+          <img src="https://img.icons8.com/ios-filled/50/000000/marker.png"/>
+          <p>Dhaka,Bangladesh</p>
+          <img src="https://img.icons8.com/ios-filled/50/000000/ringer-volume.png"/>
+          <p>+8801629104730</p>
+          <img src="https://img.icons8.com/ios-filled/50/000000/gmail-new.png"/>
+          <p>BloodInfo.gmail</p>
+      </div>
+      </div>
+    <div class="col-md-6">
+    <form action="https://formspree.io/f/mqknlkpa"method="POST">
+    <div class="form">
+        <ul>
+            <li>
+                <input type="text" name="name"placeholder="name"><br></li>
+        <li>
+              <input type="text" name="email"placeholder="Email"><br>
+        </li>
+        <li>
+             <input type="text" name="phone"placeholder="Phone No"><br>
+        </li>
+       <li> <textarea name="message"cols="23"rows="3"></textarea></li>
+       <li>
+       <input class="btn1 btn-dark" type="submit"name="submit"value="Submit">
+       </li>    
+    </div>
+    </form>
+</div>
+ </div>
+
+</div>
+<div class="container">
+  <footer>
+    <small><img src="https://img.icons8.com/ios-glyphs/30/000000/email.png"/>All right reserved. Created by Group 12.</small>
+</footer>
+</div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+<!-- <script Type="text/javascript">
+  if(window.history.replaceState){
+    window.history.replacement(null,null,window.location.href);
+  }
+</script> -->
+
+
+
+</body>
+</html>
