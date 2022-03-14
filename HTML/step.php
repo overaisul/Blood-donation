4e8c15a2e9080a37abc78f8fd0568254435741ac
merrahull <rahul.stu2018@juniv.edu>
2022-03-14 17:14:40 +0600

    Create step.php

---
+++ HTML/step.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Donation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/style.css">
+</head>
+<body>  
+    <nav class="navbar">
+        <div class="container">
+                <div class="max">
+                    <a class="nav-link active" aria-current="page" href="#">ROKTO.</a>
+                </div>
+                <ul class="nav justify-content-end">
+                   
+                    <li class="nav-item">
+                      <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="about.php">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="step.php">Steps</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="search.php">Search Donor</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="../admin/admin.php">Admin login</a>
+                      </li>
+                  </ul>
+         </div>
+        </nav>
+
+
+<div class="container">
+  <div class="mem"> 
+    <h2>How to Find a Donor</h2>
+  </div>
+  <div class="service1">
+  <div class="row">
+    <div class="col-md-4">
+      <div class="box">
+        <h2>01</h2>
+        <h3>Via SMS</h3>
+        <p>Give a message to 01629104730.</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="box">
+        <h2>02</h2>
+        <h3>Via Email</h3>
+        <p>Send a mail to Bloodinfo.gmail</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="box">
+        <h2>03</h2>
+        <h3>Via Website</h3>
+        <p>Go to contact us. Leave a message.</p>
+      </div>
+    </div>
+</div>
+<div class="container">
+    <div class="mem"> 
+    <h2>How to be a Donor</h2>
+  </div>
+  <div class="service1">
+  <div class="row">
+    <div class="col-md-4">
+      <div class="box">
+        <h2>01</h2>
+        <h3>Website</h3>
+        <p>Go the Register option in website</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="box">
+        <h2>02</h2>
+        <h3>Form</h3>
+        <p>Fill up the form with correct Information.</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="box">
+        <h2>03</h2>
+        <h3>Submit</h3>
+        <p>Click the submit button to be a donor.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+    
+
+    
+  <div class="contact">
+  <div class="con">
+    <h4>Contact Us</h4>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <ul>
+          <li>
+            <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png"/></a>
+            <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/instagram-new--v1.png"/></a>
+            <a href=""><img src="https://img.icons8.com/ios-filled/50/000000/twitter.png"/></a>
+            </li>
+        </ul>
+        <div class="footer2">
+          <img src="https://img.icons8.com/ios-filled/50/000000/marker.png"/>
+          <p>Dhaka,Bangladesh</p>
+          <img src="https://img.icons8.com/ios-filled/50/000000/ringer-volume.png"/>
+          <p>+8801629104730</p>
+          <img src="https://img.icons8.com/ios-filled/50/000000/gmail-new.png"/>
+          <p>BloodInfo.gmail</p>
+      </div>
+      </div>
+    <div class="col-md-6">
+    <form action="https://formspree.io/f/mqknlkpa"method="POST">
+    <div class="form">
+        <ul>
+            <li>
+                <input type="text" name="name"placeholder="name"><br></li>
+        <li>
+              <input type="text" name="email"placeholder="Email"><br>
+        </li>
+        <li>
+             <input type="text" name="phone"placeholder="Phone No"><br>
+        </li>
+       <li> <textarea name="message"cols="23"rows="3"></textarea></li>
+       <li>
+       <input class="btn1 btn-dark" type="submit"name="submit"value="Submit">
+       </li>    
+    </div>
+    </form>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <footer>
+    <small><img src="https://img.icons8.com/ios-glyphs/30/000000/email.png"/>All right reserved. Created by Group 12.</small>
+</footer>
+</div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+</body>
+</html>
